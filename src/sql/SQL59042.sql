f55d55f0a6411282d6822f1cc2c8d2488705f353
dvlp-sy <sy8389b@gmail.com>
2026-09-18 14:47:55 +0900

    solve: 없어진 기록 찾기

---
+++ src/sql/SQL59042.sql
@@ -0,0 +1,12 @@
+/*
+ 없어진 기록 찾기
+ https://school.programmers.co.kr/learn/courses/30/lessons/59042
+ */
+SELECT O.ANIMAL_ID AS ANIMAL_ID,
+       O.NAME AS NAME
+FROM ANIMAL_INS I
+         RIGHT OUTER JOIN ANIMAL_OUTS O
+                          ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY ANIMAL_ID, NAME
+;
